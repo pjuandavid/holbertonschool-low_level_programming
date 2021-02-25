12fc44b89ca300d416a8b41e7142ac5add3c11be
pjuandavid <pjuandavid>
2021-02-25 06:35:30 +0000

    alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_alphabet - prints the letters from a to z in lowercase
+ * Description: use _putchar.c
+ * Return: 0 H
+ */
+void print_alphabet(void);
+{
+int letra = 'a';
+while (letra <= 'z')
+{
+_putchar(letras);
+letras++;
+}
+_putchar('\n');
+}
