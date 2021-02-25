ccdee6eec33faee75050cb803d3bf4b694db4cfe
pjuandavid <pjuandavid>
2021-02-25 05:54:36 +0000

    hol

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include  "holberton.h"
+/**
+ * main - HOLBERTON
+ * Description: use _putchar.c
+ * Return:
+ */
+int main(void)
+{
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
+return (0);
+}
