e313e2c12b50214f1120da4e98eefd287f364c93
pjuandavid <pjuandavid>
2021-03-03 18:28:17 +0000

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+* _abs  - Computes the absolute value of an integer
+* Valor abs
+*@n: character
+* Return: 0 Siempre
+*/
+int _abs(int n)
+{
+if (n < 0)
+{
+return (-n);
+}
+else
+{
+return (n);
+}
+}
