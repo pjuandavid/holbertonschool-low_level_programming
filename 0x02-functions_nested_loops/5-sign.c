7a66ebb1114c2edb312aa3e3bc03fb39a724c993
pjuandavid <pjuandavid>
2021-03-03 16:31:13 +0000

    sing

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_sign - Print
+ * @n : Arbi
+ * Return: 0 V
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar ('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
