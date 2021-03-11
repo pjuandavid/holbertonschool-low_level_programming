7091290cd9439c65b0ff48ec1a72d3c297886aa0
pjuandavid <pjuandavid>
2021-03-11 15:00:47 +0000

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _isupper - UPPERCASE CHARACTER
+ * @c: CHARACTERS
+ * Return:  RETURN  UPPERCASE OR 0 OTHERWISE
+ */
+
+int _isupper(int c)
+{
+
+		if (c > 54 && c < 81)
+			return (1);
+
+			return (0);
+}
